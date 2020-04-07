71a2762a417d88871ad80afda3b78458e3a40726
ojknation <ojknation@gmail.com>
2020-04-07 23:29:35 +0100

    challenge-1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,29 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+
+    const input = data;
+    const currentlyInfectedImpact = data.reportedCases * 10;
+    const currentlyInfectedSevereImpact = data.reportedCases * 50;
+    const infectionByRequestedTimeImpact = currentlyInfectedImpact * Math.pow(2,data.timeToElapse);
+    const infectionByRequestedTimeSevereImpact = currentlyInfectedSevereImpact * Math.pow(2,data.timeToElapse);
+
+
+    let object = {
+        data : input,
+        impact: {
+            currentlyInfected : currentlyInfectedImpact,
+            infectionByRequestedTime : infectionByRequestedTimeImpact
+        },
+        severeImpact: {
+            currentlyInfected : currentlyInfectedSevereImpact,
+            infectionByRequestedTime : infectionByRequestedTimeSevereImpact
+        }
+    };
+
+    return object;
+}
+
+
 
 export default covid19ImpactEstimator;
+
+
