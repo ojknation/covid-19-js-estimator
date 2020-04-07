bab36de591e0a881ce0d2b7e33336f8b0bed730a
ojknation <ojknation@gmail.com>
2020-04-07 23:37:38 +0100

    estimator-changes-1

--- src/estimator.js
+++ src/estimator.js
@@ -1,29 +1,23 @@
 const covid19ImpactEstimator = (data) => {
-
     const input = data;
     const currentlyInfectedImpact = data.reportedCases * 10;
     const currentlyInfectedSevereImpact = data.reportedCases * 50;
-    const infectionByRequestedTimeImpact = currentlyInfectedImpact * Math.pow(2,data.timeToElapse);
-    const infectionByRequestedTimeSevereImpact = currentlyInfectedSevereImpact * Math.pow(2,data.timeToElapse);
-
-
+    const factor = data.timeToElapse / 3;
+    const infectionByRequestedTimeImpact = currentlyInfectedImpact * (2 ** factor);
+    const infectionByRequestedTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** factor);
     let object = {
         data : input,
         impact: {
-            currentlyInfected : currentlyInfectedImpact,
-            infectionByRequestedTime : infectionByRequestedTimeImpact
+        currentlyInfected : currentlyInfectedImpact,
+        nfectionByRequestedTime : infectionByRequestedTimeImpact
         },
         severeImpact: {
-            currentlyInfected : currentlyInfectedSevereImpact,
-            infectionByRequestedTime : infectionByRequestedTimeSevereImpact
+        currentlyInfected : currentlyInfectedSevereImpact,
+        infectionByRequestedTime : infectionByRequestedTimeSevereImpact
         }
     };
-
     return object;
 }
-
-
-
 export default covid19ImpactEstimator;
 
 
