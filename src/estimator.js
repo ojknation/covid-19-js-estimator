6623c19d71483bc439b90fb7add9bd602783740c
ojknation <ojknation@gmail.com>
2020-04-08 15:22:40 +0100

    rounded-1

--- src/estimator.js
+++ src/estimator.js
@@ -5,19 +5,16 @@ const covid19ImpactEstimator = (data) => {
   const currentlyInfectedSevereImpact = reportedCases * 50;
   let infectionByRequestedTimeImpact;
   let infectionByRequestedTimeSevereImpact;
-  function intConvert (value) {
-    return value | 0;
-  }
   if (periodType === 'days') {
-    const factor = intConvert(timeToElapse / 3);
+    const factor = Math.floor(timeToElapse / 3);
     infectionByRequestedTimeImpact = currentlyInfectedImpact * (2 ** factor);
     infectionByRequestedTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** factor);
   } else if (periodType === 'weeks') {
-    const factor = intConvert((timeToElapse * 7) / 3);
+    const factor = Math.floor((timeToElapse * 7) / 3);
     infectionByRequestedTimeImpact = currentlyInfectedImpact * (2 ** factor);
     infectionByRequestedTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** factor);
   } else if (periodType === 'months') {
-    const factor = intConvert((timeToElapse * 30) / 3);
+    const factor = Math.floor((timeToElapse * 30) / 3);
     infectionByRequestedTimeImpact = currentlyInfectedImpact * (2 ** factor);
     infectionByRequestedTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** factor);
   }
