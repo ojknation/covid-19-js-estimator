71873a355b5ea0adf821344840c32d6dd01cbe08
ojknation <ojknation@gmail.com>
2020-04-08 12:03:13 +0100

    estimator-changes-7

--- src/estimator.js
+++ src/estimator.js
@@ -2,17 +2,17 @@ const covid19ImpactEstimator = (data) => {
   const input = data;
   const currentlyInfectedImpact = data.reportedCases * 10;
   const currentlyInfectedSevereImpact = data.reportedCases * 50;
-  var infectionByRequestedTimeImpact;
-  var infectionByRequestedTimeSevereImpact;
-  if(data.periodType == "days"){
+  const infectionByRequestedTimeImpact
+  const infectionByRequestedTimeSevereImpact
+  if (data.periodType === 'days') {
     const factor = data.timeToElapse / 3;
     infectionByRequestedTimeImpact = currentlyInfectedImpact * (2 ** factor);
     infectionByRequestedTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** factor);
-  }else if(data.periodType == "weeks"){
+  } else if (data.periodType === 'days') {
     const factor = (data.timeToElapse * 7) / 3;
     infectionByRequestedTimeImpact = currentlyInfectedImpact * (2 ** factor);
     infectionByRequestedTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** factor);
-  }else if(data.periodType == "months"){
+  } else if (data.periodType === 'months') {
     const factor = (data.timeToElapse * 30) / 3;
     infectionByRequestedTimeImpact = currentlyInfectedImpact * (2 ** factor);
     infectionByRequestedTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** factor);
