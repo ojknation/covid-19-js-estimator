4a837b5628e5d20bb0153733ac0beb9fedb9ade7
ojknation <ojknation@gmail.com>
2020-04-10 00:15:11 +0100

    challenge3-4

--- src/estimator.js
+++ src/estimator.js
@@ -10,38 +10,38 @@ const covid19ImpactEstimator = (data) => {
   } = input;
   const currentlyInfectedImpact = reportedCases * 10;
   const currentlyInfectedSevereImpact = reportedCases * 50;
-  let infectionByRequestedTimeImpact;
-  let infectionByRequestedTimeSevereImpact;
+  let infectionBRTimeImpact;
+  let infectionBRTimeSevereImpact;
   if (periodType === 'days') {
     const factor = timeToElapse / 3;
-    infectionByRequestedTimeImpact = Math.trunc(currentlyInfectedImpact * (2 ** factor));
-    infectionByRequestedTimeSevereImpact = Math.trunc(currentlyInfectedSevereImpact * (2 ** factor));
+    infectionBRTimeImpact = Math.trunc(currentlyInfectedImpact * (2 ** factor));
+    infectionBRTimeSevereImpact = Math.trunc(currentlyInfectedSevereImpact * (2 ** factor));
   } else if (periodType === 'weeks') {
     const factor = (timeToElapse * 7) / 3;
-    infectionByRequestedTimeImpact = Math.trunc(currentlyInfectedImpact * (2 ** factor));
-    infectionByRequestedTimeSevereImpact = Math.trunc(currentlyInfectedSevereImpact * (2 ** factor));
+    infectionBRTimeImpact = Math.trunc(currentlyInfectedImpact * (2 ** factor));
+    infectionBRTimeSevereImpact = Math.trunc(currentlyInfectedSevereImpact * (2 ** factor));
   } else if (periodType === 'months') {
     const factor = (timeToElapse * 30) / 3;
-    infectionByRequestedTimeImpact = Math.trunc(currentlyInfectedImpact * (2 ** factor));
-    infectionByRequestedTimeSevereImpact = Math.trunc(currentlyInfectedSevereImpact * (2 ** factor));
+    infectionBRTimeImpact = Math.trunc(currentlyInfectedImpact * (2 ** factor));
+    infectionBRTimeSevereImpact = Math.trunc(currentlyInfectedSevereImpact * (2 ** factor));
   }
   const availableBedSpace = 0.35 * totalHospitalBeds;
-  const severeCasesImpact = Math.trunc(infectionByRequestedTimeImpact * 0.15);
-  const severeCasesSevereImpact = Math.trunc(infectionByRequestedTimeSevereImpact * 0.15);
+  const severeCasesImpact = Math.trunc(infectionBRTimeImpact * 0.15);
+  const severeCasesSevereImpact = Math.trunc(infectionBRTimeSevereImpact * 0.15);
   const hospitalBedsImpact = Math.trunc(availableBedSpace - severeCasesImpact);
   const hospitalBedsSevereImpact = Math.trunc(availableBedSpace - severeCasesSevereImpact);
-  const impactCasesForICU = Math.trunc(infectionByRequestedTimeImpact * 0.05);
-  const severeImpactCasesForICU = Math.trunc(infectionByRequestedTimeSevereImpact * 0.05);
-  const impactVentilators = Math.trunc(infectionByRequestedTimeImpact * 0.02);
+  const impactCasesForICU = Math.trunc(infectionBRTimeImpact * 0.05);
+  const severeImpactCasesForICU = Math.trunc(infectionBRTimeSevereImpact * 0.05);
+  const impactVentilators = Math.trunc(infectionBRTimeImpact * 0.02);
   const severeImpactVentilators = Math.trunc(infectionByRequestedTimeSevereImpact * 0.02);
   const populationIncome = avgDailyIncomeInUSD * avgDailyIncomePopulation;
-  const dollarsIFImpact = Math.trunc(infectionByRequestedTimeImpact * populationIncome);
-  const dollarsIFSevereImpact = Math.trunc(infectionByRequestedTimeSevereImpact * populationIncome);
+  const dollarsIFImpact = Math.trunc(infectionBRTimeImpact * populationIncome);
+  const dollarsIFSevereImpact = Math.trunc(infectionBRTimeSevereImpact * populationIncome);
   return {
     data: input,
     impact: {
       currentlyInfected: currentlyInfectedImpact,
-      infectionsByRequestedTime: infectionByRequestedTimeImpact,
+      infectionsByRequestedTime: infectionBRTimeImpact,
       severeCasesByRequestedTime: severeCasesImpact,
       hospitalBedsByRequestedTime: hospitalBedsImpact,
       casesForICUByRequestedTime: impactCasesForICU,
@@ -50,7 +50,7 @@ const covid19ImpactEstimator = (data) => {
     },
     severeImpact: {
       currentlyInfected: currentlyInfectedSevereImpact,
-      infectionsByRequestedTime: infectionByRequestedTimeSevereImpact,
+      infectionsByRequestedTime: infectionBRTimeSevereImpact,
       severeCasesByRequestedTime: severeCasesSevereImpact,
       hospitalBedsByRequestedTime: hospitalBedsSevereImpact,
       casesForICUByRequestedTime: severeImpactCasesForICU,
