a7def3e35aa772b6aee7d89c330d8915978fb9fb
ojknation <ojknation@gmail.com>
2020-04-10 02:26:50 +0100

    challenge3-13

--- src/estimator.js
+++ src/estimator.js
@@ -12,16 +12,20 @@ const covid19ImpactEstimator = (data) => {
   const currentlyInfectedSevereImpact = reportedCases * 50;
   let infectionBRTimeImpact;
   let infectionBRTimeSevereImpact;
+  let days;
   if (periodType === 'days') {
-    const factor = Math.trunc(timeToElapse / 3);
+    days = timeToElapse;
+    const factor = Math.trunc(days / 3);
     infectionBRTimeImpact = currentlyInfectedImpact * (2 ** factor);
     infectionBRTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** factor);
   } else if (periodType === 'weeks') {
-    const factor = Math.trunc((timeToElapse * 7) / 3);
+    days = timeToElapse * 7;
+    const factor = Math.trunc(days / 3);
     infectionBRTimeImpact = currentlyInfectedImpact * (2 ** factor);
     infectionBRTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** factor);
   } else if (periodType === 'months') {
-    const factor = Math.trunc((timeToElapse * 30) / 3);
+    days = timeToElapse * 30;
+    const factor = Math.trunc(days / 3);
     infectionBRTimeImpact = currentlyInfectedImpact * (2 ** factor);
     infectionBRTimeSevereImpact = currentlyInfectedSevereImpact * (2 ** factor);
   }
@@ -30,13 +34,13 @@ const covid19ImpactEstimator = (data) => {
   const severeCasesSevereImpact = Math.trunc(infectionBRTimeSevereImpact * 0.15);
   const hospitalBedsImpact = Math.trunc(availableBedSpace - severeCasesImpact);
   const hospitalBedsSevereImpact = Math.trunc(availableBedSpace - severeCasesSevereImpact);
-  const impactCasesForICU = Math.trunc(infectionBRTimeImpact * 0.05);
-  const severeImpactCasesForICU = Math.trunc(infectionBRTimeSevereImpact * 0.05);
-  const impactVentilators = Math.trunc(infectionBRTimeImpact * 0.02);
-  const severeImpactVentilators = Math.trunc(infectionBRTimeSevereImpact * 0.02);
+  const impactCasesForICU = infectionBRTimeImpact * 0.05;
+  const severeImpactCasesForICU = infectionBRTimeSevereImpact * 0.05;
+  const impactVentilators = infectionBRTimeImpact * 0.02;
+  const severeImpactVentilators = infectionBRTimeSevereImpact * 0.02;
   const populationIncome = avgDailyIncomeInUSD * avgDailyIncomePopulation;
-  const dollarsIFImpact = infectionBRTimeImpact * populationIncome * timeToElapse;
-  const dollarsIFSevereImpact = infectionBRTimeSevereImpact * populationIncome * timeToElapse;
+  const dollarsIFImpact = infectionBRTimeImpact * populationIncome * days;
+  const dollarsIFSevereImpact = infectionBRTimeSevereImpact * populationIncome * days;
   return {
     data: input,
     impact: {
