b6f059c9f27b32f3c537e50c80117d77f9387024
ojknation <ojknation@gmail.com>
2020-04-10 03:31:09 +0100

    removed unused vars

--- src/estimator.js
+++ src/estimator.js
@@ -5,11 +5,9 @@ const covid19ImpactEstimator = (data) => {
     timeToElapse,
     periodType,
     totalHospitalBeds,
-    avgDailyIncomeInUSD,
-    avgDailyIncomePopulation
   } = input;
-  const avgUSD = avgDailyIncomeInUSD;
-  const avgPop = avgDailyIncomePopulation;
+  //const avgUSD = avgDailyIncomeInUSD;
+  //const avgPop = avgDailyIncomePopulation;
   const currentlyInfectedImpact = reportedCases * 10;
   const currentlyInfectedSevereImpact = reportedCases * 50;
   let infectionBRTimeImpact;
@@ -40,8 +38,8 @@ const covid19ImpactEstimator = (data) => {
   const severeImpactCasesForICU = Math.trunc(infectionBRTimeSevereImpact * 0.05);
   const impactVentilators = Math.trunc(infectionBRTimeImpact * 0.02);
   const severeImpactVentilators = Math.trunc(infectionBRTimeSevereImpact * 0.02);
-  const dollarsIFImpact = Math.trunc(infectionBRTimeImpact * avgUSD * avgPop * days);
-  const dollarsIFSevereImpact = Math.trunc(infectionBRTimeSevereImpact * avgUSD * avgPop * days);
+  //const dollarsIFImpact = Math.trunc(infectionBRTimeImpact * avgUSD * avgPop * days);
+  //const dollarsIFSevereImpact = Math.trunc(infectionBRTimeSevereImpact * avgUSD * avgPop * days);
   return {
     data: input,
     impact: {
